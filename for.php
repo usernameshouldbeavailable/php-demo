8ff668238ac85f5f9150f114dd6d88e2b9d17b0c
usernameshouldbeavailable <avinash@edunetfoundation.org>
2021-11-23 17:30:50 +0530

    for loop

---
+++ for.php
@@ -0,0 +1,15 @@
+<?php
+
+    $names = ["Avinash", "Sachin", "Virat"];
+
+    echo "<ol>";
+
+    for ( $i = 0; $i < 3; $i++ ) {
+
+        echo "<li>" . $names[$i] . "</li>";
+
+    }
+
+    echo "</ol>";
+
+?>
