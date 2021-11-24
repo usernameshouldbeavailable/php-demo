ceb6426e1b6a43d17ded446ff4b747a14fcfdf2f
usernameshouldbeavailable <avinash@edunetfoundation.org>
2021-11-24 16:51:04 +0530

    foreach loop example

---
+++ foreach.php
@@ -0,0 +1,16 @@
+<?php
+
+    $names = array(
+        "Avinash", 
+        "Nokia", 
+        "Virat", 
+        "Sachin", 
+        "Dhoni"
+    );
+
+    foreach($names as $name)
+    {
+        echo $name . '<br />';
+    }
+
+?>
